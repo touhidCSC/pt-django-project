7b6fa078a798544a8950a3efd9f17f63e2c7ad10
touhidCSC <touhiduli9899@gmail.com>
2023-10-19 21:35:47 +0600

    dynamic html done

--- PtFinalProject/Blog/views.py
+++ PtFinalProject/Blog/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
 
-# Create your views here.
+blog_post = [
+    {
+        'title': "সেই বিশ্বকাপ, সেই ভারত, অন্য তামিম",
+        'content': ("তামিম ইকবালকে ক্রিকেট–বিশ্ব কিভাবে চিনেছিল? চিনেছিল বিশ্বকাপে ভারতের বিপক্ষে "
+                    "৫১ রানের একটা ইনিংসে, ২০০৭ বিশ্বকাপে পোর্ট"),
+        'author': "মাশরাফি",
+        "post_create datetime": " ১৯ অক্টোবর ২০২৩, ১৯: ২৭",
+    }
+
+
+]
+
+
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {"blog_post": blog_post})
